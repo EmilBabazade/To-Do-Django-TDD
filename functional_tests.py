07bb63cc6bfec7d50218c13ebaf3c91928e0ac12
Emil <emilbabazade29@gmail.com>
2020-01-06 22:35:30 +0200

    First FT specced out in comments, and now uses unittest.

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,38 @@
 from selenium import webdriver
+import unittest
 
-path = 'C:/Users/emilb/Documents/geckodriver.exe'
-browser = webdriver.Firefox(executable_path=path)
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.geckodriver_path = 'C:/Users/emilb/Documents/geckodriver.exe'
+        self.browser = webdriver.Firefox(executable_path=self.geckodriver_path)
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Eddy and Ed bothered Edddy about an online to-do website so much he went to check it
+        # so they would shut up
+        self.browser.get('http://localhost:8000')
+
+        # He noticed page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('You haven\'t finished writing this test!')
+
+        # He is invited to enter a to-do item straight away
+
+        # He types "Buy peacock feathers" ( he likes tying fly-fishing lures when he is stoned )
+
+        # When he hits Enter, the page updates, and now page lists
+        #  '1: Buy peacock feathers' as a to-do item in the list
+
+        # There is still a text box inviting him to enter another to-do item.
+        # He types 'Use peacock feathers to make a fly' ( he is unexpectedly methodical)
+
+        # The page updates again and now he can see both to-do items in the list
+
+        # Ed wonders wether the site will remember his list. Then he sees that the site 
+        # has generated a unique URL for him -- there is some explanatory text to that effect
+
+        # He visits the URL - his to-do list is alive and well staring at his face
+
+unittest.main()
