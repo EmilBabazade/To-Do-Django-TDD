74ae64ebe1d0ef12c363bf72d6baf484835204c5
Emil <emilbabazade29@gmail.com>
2020-01-15 06:35:20 +0200

    Small refactoring

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_text_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Eddy and Ed bothered Edddy about an online to-do website so much he went to check it
         # so they would shut up
@@ -35,10 +40,7 @@ class NewVisitorTest(unittest.TestCase):
         #  '1: Buy peacock feathers' as a to-do item in the list
         input_box.send_keys(Keys.ENTER)
         time.sleep(1) # wait for page to update
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers.', [row.text for row in rows])
-
+        self.check_for_text_in_list_table('1: Buy peacock feathers.')
         # There is still a text box inviting him to enter another to-do item.
         # He types 'Use peacock feathers to make a fly.' ( he is unexpectedly methodical)
         input_box = self.browser.find_element_by_id('id_new_item')
@@ -47,14 +49,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page updates again and now he can see both to-do items in the list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers.', [row.text for row in rows])
-        self.assertIn(
-                '2: Use peacock feathers to make a fly.',
-                [row.text for row in rows]
-            )
-
+        self.check_for_text_in_list_table('1: Buy peacock feathers.')
+        self.check_for_text_in_list_table('2: Use peacock feathers to make a fly.')
         # Ed wonders wether the site will remember his list. Then he sees that the site 
         # has generated a unique URL for him -- there is some explanatory text to that effect
         self.fail('Finish the test!')
